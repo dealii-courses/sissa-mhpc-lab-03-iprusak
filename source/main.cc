15be897b9cc2ee0850f23bb991a5cfd0c4459e74
Ivan Prusak <iv.prusak@gmail.com>
2021-04-07 14:33:02 +0200

    q

--- source/main.cc
+++ source/main.cc
@@ -7,14 +7,14 @@ main()
   Step3 laplace_problem;
   laplace_problem.run("parameters.prm");
   Step3 laplace_problem_ball;
-  laplace_problem_ball.run("parameters/hyper_ball.prm");
+  laplace_problem_ball.run("hyper_ball.prm");
   Step3 laplace_problem_cube;
-  laplace_problem_cube.run("parameters/hyper_cube.prm");
+  laplace_problem_cube.run("hyper_cube.prm");
   Step3 laplace_problem_shell;
-  laplace_problem_shell.run("parameters/hyper_shell.prm");
+  laplace_problem_shell.run("hyper_shell.prm");
   Step3 laplace_problem_sinbc;
-  laplace_problem_sinbc.run("parameters/sin_bc.prm");
+  laplace_problem_sinbc.run("sin_bc.prm");
   Step3 laplace_problem_sinrhs;
-  laplace_problem_sinrhs.run("parameters/sin_rhs.prm");
+  laplace_problem_sinrhs.run("sin_rhs.prm");
   return 0;
 }
--- tests/step-3-tester.cc
+++ tests/step-3-tester.cc
@@ -16,24 +16,24 @@ public:
 
 TEST_F(Step3Tester, HyperCube)
 {
-  make_grid("hyper_cube.prm");
+  make_grid("../parameters/hyper_cube.prm");
 }
 
 TEST_F(Step3Tester, HyperBall)
 {
-  make_grid("hyper_ball.prm");
+  make_grid("../parameters/hyper_ball.prm");
 }
 
 
 TEST_F(Step3Tester, HyperShell)
 {
-  make_grid("hyper_shell.prm");
+  make_grid("../parameters/hyper_shell.prm");
 }
 TEST_F(Step3Tester, SinBC)
 {
-  make_grid("sin_bc.prm");
+  make_grid("../parameters/sin_bc.prm");
 }
 TEST_F(Step3Tester, SinRHS)
 {
-  make_grid("sin_rhs.prm");
+  make_grid("../parameters/sin_rhs.prm");
 }
